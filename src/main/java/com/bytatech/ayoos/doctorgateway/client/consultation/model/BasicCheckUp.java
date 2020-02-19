0166c3b1a84fe292dff2ef9eeb7f9bc45cf18e22
sarathadoor <sarath.kumar.s@lxisoft.com>
2020-02-19 14:46:02 +0530

    Refactord Initiate Method

--- src/main/java/com/bytatech/ayoos/doctorgateway/client/consultation/model/BasicCheckUp.java
+++ src/main/java/com/bytatech/ayoos/doctorgateway/client/consultation/model/BasicCheckUp.java
@@ -13,7 +13,7 @@ import javax.validation.constraints.*;
  * BasicCheckUp
  */
 @Validated
-@javax.annotation.Generated(value = "org.openapitools.codegen.languages.SpringCodegen", date = "2020-02-19T11:36:48.535+05:30[Asia/Calcutta]")
+@javax.annotation.Generated(value = "org.openapitools.codegen.languages.SpringCodegen", date = "2020-02-19T14:36:23.395+05:30[Asia/Calcutta]")
 
 public class BasicCheckUp   {
   @JsonProperty("bp")
